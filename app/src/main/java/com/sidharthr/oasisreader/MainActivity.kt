60950740749d911e1bf1445bba4f02aa31e4d9b1
Sidharth Raja <sidharth15.raja@gmail.com>
2023-10-30 09:55:38 -0700

    cleaned up homepage

--- app/src/main/java/com/sidharthr/oasisreader/MainActivity.kt
+++ app/src/main/java/com/sidharthr/oasisreader/MainActivity.kt
@@ -12,20 +12,28 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideOut
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -58,13 +66,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
                         startDestination = "home") {
                         composable("home") {
                             LazyColumn(
+//                                modifier=Modifier.verticalScroll(rememberScrollState()),
                                 content = {
                                     item {
                                         Text(
@@ -90,8 +98,6 @@ class MainActivity : ComponentActivity() {
                             Text(text = "Article")
                         }
                     }
-
-
                 }
             }
         }
@@ -101,22 +107,28 @@ class MainActivity : ComponentActivity() {
 // A composable card of an article, contains the title, excerpt, and date.
 @Composable
 fun ArticleCard(articleItem: ArticleItem, onClick: () -> Unit = {}) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .clickable { onClick() },
-    ) {
-        Column {
-            Text(
-                text = articleItem.title,
-                modifier = Modifier.padding(16.dp)
-            )
-            Text(
-                text = articleItem.excerpt,
-                modifier = Modifier.padding(16.dp)
-            )
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 8.dp)
+        .clickable { onClick() }){
+        Text(
+            modifier = Modifier.padding(bottom=2.dp),
+            fontSize=20.sp,
+            text = articleItem.title
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(modifier= Modifier
+                .border(1.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
+                .padding(horizontal = 8.dp, vertical = 2.dp)
+                .alpha(0.6f),
+                fontSize = 16.sp, text="theinformation.com")
+            Text(modifier= Modifier
+                .padding(horizontal = 8.dp)
+                .alpha(0.8f), fontSize = 16.sp, text="5 hours ago")
         }
+
+
     }
 }
 
--- app/src/main/java/com/sidharthr/oasisreader/ui/theme/Theme.kt
+++ app/src/main/java/com/sidharthr/oasisreader/ui/theme/Theme.kt
@@ -57,7 +57,7 @@ fun OasisReaderTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
